ce2d2423d62536f318a08c423fd3a0c5cc0d6cbb
Mark Pentland <mpentland@deconetwork.com>
2025-05-20 09:45:51 +1000

    set socket as non-blocking

--- lib/MogileFS/Connection/Client.pm
+++ lib/MogileFS/Connection/Client.pm
@@ -7,13 +7,14 @@ package MogileFS::Connection::Client;
 use strict;
 use Danga::Socket ();
 use base qw{Danga::Socket};
-
+use IO::Handle;
 use fields qw{read_buf};
 
 sub new {
     my $self = shift;
     $self = fields::new($self) unless ref $self;
     $self->SUPER::new( @_ );
+    IO::Handle::blocking($self->{sock}, 0);
     $self->watch_read(1);
     return $self;
 }
